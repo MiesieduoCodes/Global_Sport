2d286d7670af53fef821652d5b5f05ae262d7e49
Miesieduo Veria <miesieduoveria@gmail.com>
2025-02-19 11:55:57 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,12 +5,8 @@ const nextConfig = {
       'images.unsplash.com',
       'instagram.flos5-2.fna.fbcdn.net',
     ],
-  },
-  i18n: {
-    locales: ['en', 'es'], // Add your supported locales here
-    defaultLocale: 'en', // Set your default locale
-  },
+  }
 };
 
-// Export the configuration with the Next Intl plugin
-export default withNextIntl(nextConfig);
+// Export the configuration directly
+export default nextConfig;
